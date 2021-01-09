6ee76e75e8476858125c2d959f67dec39cb0e088
mnazartyagi <mnazartyagi@outlook.com>
2021-01-09 07:55:25 +0530

    Code Refactor

--- src/app/movies/movie.ts
+++ src/app/movies/movie.ts
@@ -1,41 +1,40 @@
-export class Movie {
-  constructor(
-    public adult : boolean,
-    public backdrop_path : string,
-    public budget : number,
-    public genres: {
+export interface Movie {
+     adult : boolean,
+   backdrop_path : string,
+   budget : number,
+   genres: {
       id: number,
       name: string
     }[],
-    public genre_ids : number,
-    public homepage: string,
-    public overview : string,
-    public id : number,
-    public imdb_id : string,
-    public original_language : string,
-    public original_title : string,
-    public ovierview : string,
-    public popularity : number,
-    public poster_path : string,
-    public production_companies: {
+   genre_ids : number,
+   homepage: string,
+   overview : string,
+   id : number,
+   imdb_id : string,
+   original_language : string,
+   original_title : string,
+   ovierview : string,
+   popularity : number,
+   poster_path : string,
+   production_companies: {
       id: number,
       name: string
     }[],
-    public production_countries: {
+   production_countries: {
       iso_3166_1: string,
       name: string
     }[],
-    public release_date : string,
-    public revenue : number,
-    public runtime : number,
-    public spoken_languages: {
+   release_date : string,
+   revenue : number,
+   runtime : number,
+   spoken_languages: {
       iso_3166_1: string,
       name: string
     }[],
-    public status : string,
-    public title : string,
-    public tagline : string,
-    public video : boolean,
-    public vote_average : number,
-    public vote_count : number) { }
+   status : string,
+   title : string,
+   tagline : string,
+   video : boolean,
+   vote_average : number,
+   vote_count : number
 }
